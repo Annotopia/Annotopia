b78ad12c545900cd558de66c8edd1202be9dd3ea
Paolo Ciccarese <paolo.ciccarese@gmail.com>
2014-06-16 12:03:37 -0400

    Enabled token refresh

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,9 +1,11 @@
 import org.annotopia.grails.security.LoggingSecurityEventListener
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.commonsemantics.grails.security.oauth.OAuthAuthorizationCodeTokenGranter;
-import org.commonsemantics.grails.security.oauth.OAuthClientDetailsService;
-import org.commonsemantics.grails.security.oauth.OAuthTokenStore;
-import org.springframework.security.oauth2.provider.code.InMemoryAuthorizationCodeServices;
+import org.commonsemantics.grails.security.oauth.OAuthAuthorizationCodeTokenGranter
+import org.commonsemantics.grails.security.oauth.OAuthClientDetailsService
+import org.commonsemantics.grails.security.oauth.OAuthTokenStore
+import org.springframework.security.oauth2.provider.CompositeTokenGranter
+import org.springframework.security.oauth2.provider.code.InMemoryAuthorizationCodeServices
+import org.springframework.security.oauth2.provider.refresh.RefreshTokenGranter
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices
 
 //import org.annotopia.grails.services.CustomUserDetailsService
 // Place your Spring DSL code here
@@ -27,11 +29,16 @@ beans = {
 
 	authorizationCodeServices(InMemoryAuthorizationCodeServices)
 
-	oauth2TokenGranter(OAuthAuthorizationCodeTokenGranter,
-		tokenServices = ref("tokenServices"),
-		authorizationCodeServices = ref("authorizationCodeServices"),
-		clientDetailsService = ref("clientDetailsService"))
-	{
-	}
+	oauth2TokenGranter1(OAuthAuthorizationCodeTokenGranter,
+            tokenServices = ref("tokenServices"),
+            authorizationCodeServices = ref("authorizationCodeServices"),
+            clientDetailsService = ref("clientDetailsService"))
+
+    oauth2TokenGranter2(RefreshTokenGranter,
+            tokenServices = ref("tokenServices"),
+            clientDetailsService = ref("clientDetailsService"))
+
+    oauth2TokenGranter(CompositeTokenGranter, [ref("oauth2TokenGranter1"), ref("oauth2TokenGranter2")])
+
 
 }
