4f46de8fd3c0da5ffaf630396cfeaefa732fb3c8
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-04-20 14:35:23 +0100

    Switched config to use port 8090 as given in examples

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,6 +31,8 @@ grails.project.source.level = 1.6
 //   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
 //]
 
+grails.server.port.http = 8090
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
