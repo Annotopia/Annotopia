d09777e46bbd2bfcbcdfa97f9b48f1acfaa20d26
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-05-05 13:40:54 +0100

    Made a proper URL for new PUT API

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,15 +1,19 @@
 class UrlMappings {
 
 	static mappings = {
-		// New "incremental" API
+		// New "RESTful"/"incremental" API
 		"/s/annotationset/$id/annotations"{
 			controller = "openAnnotationSetREST"
-			action = [POST:"createAnnotation"]
+			action = [POST:"createAnnotation", PUT:"replaceAnnotationSet"]
 		}
 		// Storage
 		"/s/annotationset/$id?"{
 			controller = "annotationIntegrated"
-			action = [GET:"showAnnotationSet", POST:"saveAnnotationSet", PUT:"updateAnnotationSet"]
+			action = [GET:"show", POST:"create", PUT:"update"]
+		}
+		"/s/annotationset"{
+			controller = "annotationIntegrated"
+			action = [GET:"index"]
 		}
 		"/s/annotation/$id?"{
 			controller = "openAnnotation"       /* Plain single Open Annotation */
@@ -17,40 +21,40 @@ class UrlMappings {
 			/*controller = "annotationIntegrated"*/ /* Domeo-Utopia */
 			action = [GET:"show", POST:"save", PUT:"update", DELETE:"delete"]
 		}
-		
+
 		// BioPortal Connector
 		"/cn/bioportal/search"{
 			controller = "bioPortal"
 			action = [GET:"search"]
 		}
-		
+
 		"/cn/bioportal/textmine"{
 			controller = "bioPortal"
 			action = [POST:"textmine"]
 		}
-		
+
 		"/cn/bioportal/vocabularies"{
 			controller = "bioPortal"
 			action = [GET:"vocabularies"]
 		}
-		
+
 		"/cn/nif/textmine"{
 			controller = "nif"
 			action = [POST:"textmine"]
 		}
-		
+
 		"/cn/nif/search"{
 			controller = "nif"
 			action = [GET:"search"]
 		}
-		
+
 		"/cn/ebi/textmine"{
 			controller = "ebi"
 			action = [POST:"textmine"]
 		}
-		
+
 		"/cn/pubmed/$action" (controller: "pubmed")
-		
+
 		// Validation
 		"/oa/validate"{
 			controller = "openAnnotation"
@@ -60,11 +64,11 @@ class UrlMappings {
 			controller = "openAnnotation"
 			action = "search"
 		}
-		
+
 		"/api/stats/$action?" {
 			controller = "openAnnotationReporting"
 		}
-		
+
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
